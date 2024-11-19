eab13755699b94a4cf7cde4fa3eca96e5373c574
Mikhail Churikov <churikov-20@yandex.ru>
2024-11-19 12:03:59 +0300

    refactor  gradle

--- src/test/java/ru/senla/user/RunCucumberTest.java
+++ src/test/java/ru/senla/user/RunCucumberTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/scenarios",
-        plugin = {"json:build/cucumber.json"},
+        plugin = {"json:build/resources/cucumber.json"},
         glue = {"ru.senla.user"},
         tags = "@smoke",
         snippets = CucumberOptions.SnippetType.UNDERSCORE
